0bdec8f41ca9f8d97cfedda888bc131f5cfa38a3
Pitchiu <69166155+Pitchiu@users.noreply.github.com>
2022-10-08 13:44:39 +0200

    Add closing polygons
    Whole polygons can be added. Clicking on first point while creating polygon will close it. Distancing from existing line added.

--- Source/scribblearea.cpp
+++ Source/scribblearea.cpp
@@ -6,24 +6,85 @@ ScribbleArea::ScribbleArea(QWidget *parent)
 {
     // Roots the widget to the top left even if resized
     setAttribute(Qt::WA_StaticContents);
-
-    // Set defaults for the monitored variables
-    modified = false;
-    scribbling = false;
-    myPenWidth = 1;
+    setMouseTracking(true);
+    myPenWidth = 5;
     myPenColor = Qt::blue;
+    mode = Off;
+    activePolygon = NULL;
 }
 
+void ScribbleArea::setPenColor(const QColor &newColor)
+{
+    myPenColor = newColor;
+}
+
+void ScribbleArea::setPenWidth(int newWidth)
+{
+    myPenWidth = newWidth;
+}
+
+void ScribbleArea::clearImage()
+{
+    image.fill(qRgb(255, 255, 255));
+    update();
+}
+
+void ScribbleArea::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        if(activePolygon!=NULL && distance(event->pos(), activePolygon->Points.first()) < MINDIST)
+        {
+            mode = Off;
+            activePolygon->isClosed = true;
+            activePolygon->Lines << QLine(activePolygon->Points.last(), activePolygon->Points.first());
+            activePolygon = NULL;
+            return;
+        }
+
+        for(auto polIt = Polygons.begin(); polIt != Polygons.end(); ++polIt)
+            for(auto lineIt = polIt->Lines.begin(); lineIt != polIt->Lines.end(); ++lineIt)
+                if(distance(event->pos(), *lineIt) < MINDIST) return;
+
+        if (mode==Off)
+        {
+            mode = Scribbling;
+        }
+        else if(activePolygon == NULL)
+        {
+            Polygon p;
+            p.Points << startPoint << event->pos();
+            p.Lines << QLine(startPoint,event->pos());
+            Polygons << p;
+            activePolygon = &Polygons.last();
+        }
+        else if(event->pos() != activePolygon->Points.first())
+        {
+            activePolygon->Lines << QLine(activePolygon->Points.last(), event->pos());
+            activePolygon->Points << event->pos();
+        }
+        startPoint = event->pos();
+    }
+}
+
+void ScribbleArea::mouseMoveEvent(QMouseEvent *event)
+{
+    if (mode == Scribbling)
+    {
+        lastPoint = event->pos();
+        drawImage();
+    }
+}
+
+void ScribbleArea::mouseReleaseEvent(QMouseEvent *event)
+{
+
+}
 
 void ScribbleArea::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
-
-    // Returns the rectangle that needs to be updated
     QRect dirtyRect = event->rect();
-
-    // Draws the rectangle where the image needs to
-    // be updated
     painter.drawImage(dirtyRect, image, dirtyRect);
 }
 
@@ -32,36 +93,73 @@ void ScribbleArea::drawPoint(const QPoint &endPoint)
 
 }
 
-void ScribbleArea::mousePressEvent(QMouseEvent *event)
+void ScribbleArea::drawImage()
 {
-    if (event->button() == Qt::LeftButton) {
-        lastPoint = event->pos();
-        scribbling = true;
-    }
+    image.fill(qRgb(255, 255, 255));
+    QPainter painter(&image);
+    painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,
+                        Qt::RoundJoin));
+    for(auto it = Polygons.begin(); it!=Polygons.end(); ++it)
+    painter.drawLines(it->Lines);
+    if(mode==Scribbling)
+        painter.drawLine(startPoint, lastPoint);
     update();
 }
 
-void ScribbleArea::drawLineTo(const QPoint &endPoint)
+void ScribbleArea::drawLine(const QPoint &startPoint, const QPoint &endPoint)
 {
-    // Used to draw on the widget
-    QPainter painter(this);
-
-    // Set the current settings for the pen
+    QPainter painter(&image);
     painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,
                         Qt::RoundJoin));
-
-    // Draw a line from the last registered point to the current
     painter.drawLine(lastPoint, endPoint);
 
-    // Set that the image hasn't been saved
-    modified = true;
-
     int rad = (myPenWidth / 2) + 2;
+    update(QRect(lastPoint, endPoint).normalized().adjusted(-rad, -rad, +rad, +rad));
+}
 
-    // Call to update the rectangular space where we drew
-    update(QRect(lastPoint, endPoint).normalized()
-                                     .adjusted(-rad, -rad, +rad, +rad));
+void ScribbleArea::resizeImage(QImage *image, const QSize &newSize)
+{
+    if (image->size() == newSize)
+        return;
 
-    // Update the last position where we left off drawing
-    lastPoint = endPoint;
+    QImage newImage(newSize, QImage::Format_RGB32);
+    newImage.fill(qRgb(255, 255, 255));
+    QPainter painter(&newImage);
+    painter.drawImage(QPoint(0, 0), *image);
+    *image = newImage;
+}
+
+void ScribbleArea::resizeEvent(QResizeEvent *event)
+{
+    if (width() > image.width() || height() > image.height()) {
+        int newWidth = qMax(width() + 128, image.width());
+        int newHeight = qMax(height() + 128, image.height());
+        resizeImage(&image, QSize(newWidth, newHeight));
+        update();
+    }
+    QWidget::resizeEvent(event);
+}
+
+//this functions should be moved to some helper class
+qreal ScribbleArea::distance(QPointF a, QPointF b)
+{
+    return QLineF(a, b).length();
+}
+
+QPointF nearestPoint(const QLineF& line, const QPointF& point);
+
+//QPointF nearestPoint(const QLineF& line, const QPointF& point);
+qreal ScribbleArea::distance(QPoint a, QLineF b)
+{
+    qreal dist = distance(a, nearestPoint(b, a));
+    return dist;
+}
+
+QPointF nearestPoint(const QLineF& line, const QPointF& point)
+{
+QLineF perpendicLine(point,QPointF(point.x(),0.0));
+perpendicLine.setAngle(90.0+line.angle());
+QPointF result;
+line.intersects(perpendicLine,&result);
+return result;
 }
--- Source/scribblearea.h
+++ Source/scribblearea.h
@@ -4,56 +4,74 @@
 #include <QImage>
 #include <QPoint>
 #include <QWidget>
+#include <QList>
+
+#define MINDIST 10
+
+class Polygon;
 
 class ScribbleArea : public QWidget
 {
-    // Declares our class as a QObject which is the base class
-    // for all Qt objects
-    // QObjects handle events
     Q_OBJECT
 
 public:
     ScribbleArea(QWidget *parent = 0);
+    void setPenColor(const QColor &newColor);
+    void setPenWidth(int newWidth);
 
-    // Has the image been modified since last save
     bool isModified() const { return modified; }
     QColor penColor() const { return myPenColor; }
+    int penWidth() const { return myPenWidth; }
+
+public slots:
+    void clearImage();
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
-    //void mouseMoveEvent(QMouseEvent *event) override;
-    //void mouseReleaseEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void drawPoint(const QPoint &endPoint);
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent *event);
 
-    // Updates the scribble area where we are painting
-    //void paintEvent(QPaintEvent *event) override;
-
-    // Makes sure the area we are drawing on remains
-    // as large as the widget
-    //void resizeEvent(QResizeEvent *event) override;
+    enum Mode
+    {
+        Off,
+        Scribbling
+    };
 
 private:
-    void drawLineTo(const QPoint &endPoint);
-    void drawPoint(const QPoint &endPoint);
-    // Will be marked true or false depending on if
-    // we have saved after a change
+    void drawLine(const QPoint &startPoint, const QPoint &endPoint);
+    void resizeImage(QImage *image, const QSize &newSize);
+    void drawImage();
+
     bool modified;
-
-    // Marked true or false depending on if the user
-    // is drawing
     bool scribbling;
-
-    // Holds the current pen width & color
     int myPenWidth;
 
+    //this should be moved
+    qreal distance(QPointF a, QPointF b);
+    qreal distance(QPoint a, QLineF b);
+
     QColor myPenColor;
-
-    // Stores the image being drawn
     QImage image;
-
-    // Stores the location at the current mouse event
+    QPoint startPoint;
     QPoint lastPoint;
+    Polygon* activePolygon;
+
+    QList<Polygon> Polygons;
+    Mode mode;
 };
+
+class Polygon
+{
+public:
+    bool isClosed = false;
+    QList<QPoint> Points;
+    QList<QLine> Lines;
+};
+
+
 #endif // SCRIBBLEAREA_H
 
 
